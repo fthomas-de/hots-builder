5a61a84d5f71b669d288bd4ac970ca78d6c29d07
Trudikampfschaf <flothomas@web.de>
2014-09-15 16:03:57 +0200

    wip

--- web/app/views.py
+++ web/app/views.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, url_for, request, redirect
 from app import app
+
 @app.route('/')
 @app.route('/hots')
 @app.route('/hots/builder')
 def index():
 	return render_template('index.html', page='index')
+
+@app.route('/create')
+def create():
+	return render_template('create.html', page='create')
