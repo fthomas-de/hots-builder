dc6e52a96d98af0255f3b283cfbdad9750bb02a9
Trudikampfschaf <flothomas@web.de>
2014-09-16 18:21:57 +0200

    daily save

--- web/app/views.py
+++ web/app/views.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template, url_for, request, redirect
 from app import app
+from os import walk
+
+path = '/home/fthomas/Dokumente/hots-builder/web/app/static/img/heroes'
+(_, _, hero_images_tmp) = walk(path).next()
+
+hero_images = []
+hero_images.append(hero_images_tmp[:10])
+hero_images.append(hero_images_tmp[10:19])
+hero_images.append(hero_images_tmp[19:])
 
 @app.route('/')
 @app.route('/hots')
@@ -9,4 +18,8 @@ def index():
 
 @app.route('/create')
 def create():
-	return render_template('create.html', page='create')
+	return render_template('create.html', page='create', img=hero_images)
+
+@app.route('/<name>')
+def build(name):
+	return render_template('build.html', page='build', name=name)
